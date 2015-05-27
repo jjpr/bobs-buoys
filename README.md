ea45834a5ad1adb3e76fd3e93303476da42c5d28
Jonathan J. Prescott-Roy <jjprescottroy@gmail.com>
2015-05-27 14:23:43 -0400

    Added HTML, changed directory format.

---
+++ README.md
@@ -0,0 +1,3 @@
+Server Code for Buoy Project
+============================
+This repository holds the code for the server which will run on the buoys.  
--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-var express    = require('express')
-var serveIndex = require('serve-index')
+var express    = require('express');
+var serveIndex = require('serve-index');
+var path = require('path');
 
-var app = express()
+var app = express();
 
-app.use(express.static("data"))
-app.use('/', serveIndex('data', {'icons': true}))
-app.listen(8000)
+app.use('/data', express.static("data"));
+app.use('/data', serveIndex('data', {'icons': true, 'view': 'details'}));
+app.get("/", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+app.listen(8000);
